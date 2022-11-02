59f6b5ce0310e086ed53cd04cb5fd2efe5a0b9d8
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2022-11-02 11:36:16 +0530

    Prime Problem

---
+++ prime_problem.js
@@ -0,0 +1,20 @@
+function checkPrime(number){
+    let factors=0;
+     for(a=1;a>=number;a--){
+      if(number%a==0){
+       factors+;
+      }
+     }
+if(factors==2){
+ return true;
+}else{
+ return false;
+}
+}
+
+let result=checkPrime(13);
+if(result==true){
+console.log("Prime Number");
+}else{
+console.log("Not a Prime Number");
+}
